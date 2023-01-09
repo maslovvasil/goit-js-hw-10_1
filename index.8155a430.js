fed1f1e7e69f24ec4cda386e0d083eb223df8922
maslovvasil <93223232+maslovvasil@users.noreply.github.com>
2023-01-09 14:05:12 +0000

    Deploying to gh-pages from @ maslovvasil/goit-js-hw-10_1@d74a952d00d733c36dc78b820b17586718495204 🚀

---
+++ index.8155a430.js
@@ -0,0 +1,2 @@
+$(".owl-carousel").owlCarousel({loop:!0,margin:10,nav:!0,navText:['<span class="arrow-owl arrow-right"> <<< </span>','<span class="arrow-owl arrow-left"> >>> </span>'],responsive:{0:{items:1},600:{items:3},1e3:{items:6}}});
+//# sourceMappingURL=index.8155a430.js.map
